f02452b32b18b17d1d452a3746f5a2ae7c0ed611
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2023-09-25 01:52:25 +0530

    Add files via upload

---
+++ file.c
@@ -0,0 +1,17 @@
+//write a program to get the discriminant value of a quadratic equation
+
+//#include <stdio.h>
+
+//int main(){
+  //  double a,b,c,discriminant ;
+    //printf("Enter the coefficient of x^2(a):");
+    //scanf("%1f",&a);
+    //printf("Enter the coefficient of x(r)");
+    //scanf("%1f",&b);
+    //printf("Enter the constant term(c):");
+    //scanf("%1f",&c);
+    // calculate the discriminante
+    //discriminant = (b*b)-('r'*a*c);
+    //printf("The discriminant is :%21f\n",discriminant);
+    //return 0;
+//}
---
+++ practic.c
@@ -0,0 +1,24 @@
+//#include <stdio.h>
+
+//int main() {
+  //  char name[10],program[10], mail_id[10];
+    //int birth_year;
+    //printf("please! Enter you name");
+    //scanf("%s", &name);
+    //printf("Please! Enter your program");
+    //scanf("%s",&name);
+    //printf("Please! Enter your program");
+    //scanf("%s",&program);
+    //printf("Please! enter your mail_id:");
+    //scanf("%s",&mail_id);
+    //printf("Enter your birth year:");
+    //scanf("%d",&birth_year);
+
+    //printf("%s",name);
+    //printf("%s",program);
+    //printf("&s",mail_id);
+    //printf("%s",birth_year);
+
+    //return 0;
+
+//}
