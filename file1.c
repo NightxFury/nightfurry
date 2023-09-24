61901a89f3e1bf55e82cd0370471e13fabe6894e
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2023-09-25 03:12:46 +0530

    Add files via upload

---
+++ file1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int row,sopace,star;
+    printf("Enter the number of rows for the pyramid:");
+    scanf("%d",&row);
+     for(int i = 1; i<= row; i++);
+     //print spaces
+     for(sopace = 1);
+
+
+}
