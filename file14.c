14658f991f22277da7302c44946d560ecb2ce7ca
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2023-10-01 23:52:42 +0530

    Add files via upload

---
+++ file14.c
@@ -0,0 +1,18 @@
+//
+
+//#include <stdio.h>
+
+//int main () {
+    //float tempreture;
+
+    //printf("Enter the temprature in celsius :");
+    //scanf("%f", &tempreture);
+
+    //if(tempreture >= 100.00){
+      //  printf("Water can reach its boiling point at this temperature.\n");
+
+    //}else{
+      //  printf("Water cannot reach its boling point at this temperature.\n");
+    //}
+  //  return 0;
+//}
---
+++ file15.c
@@ -0,0 +1,8 @@
+//
+
+//#include <stdio.h>
+
+//int main () {
+  //  float p, r, t, si , ci
+
+//}
