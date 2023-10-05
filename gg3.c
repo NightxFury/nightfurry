163b32f1294860124b6ad4529ed6a1a7a8eeddce
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2023-10-05 23:59:00 +0530

    Add files via upload

---
+++ gg3.c
@@ -0,0 +1,17 @@
+//
+
+#include <stdio.h>
+
+int main () {
+    //declaration / prototype
+    void printfhello ();
+
+    int main () {
+        printf hello(); //function call
+        return 0;
+    }
+    //function definition
+    void printfhello () {
+        printf("Hello");
+    }
+}
