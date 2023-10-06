40b957afcc381cc4f68c2b4da1b9da028e26a159
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2023-10-06 09:24:17 +0530

    Add files via upload

---
+++ assignment4.c
@@ -0,0 +1,16 @@
+//Question 2>>>>
+//
+#include <stdio.h>
+
+int main() {
+    int n = 5; // Number of rows
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
