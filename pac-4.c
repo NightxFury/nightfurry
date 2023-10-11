e8d9139682239206f11eb7b90a1f1311a4cb2cb8
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2023-10-12 00:03:30 +0530

    Add files via upload

---
+++ pac-4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main () {
+    int A,B, sum = 0;
+
+    printf("Enter two number A & B ");
+
+    scanf("%d%d", &A, &B);
+
+
+    sum = A +B;
+
+    printf("Sum of A and B is : %d", sum);
+
+    return 0;
+
+}
