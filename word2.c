5551f27915f0b7fa09f1242c5eb30ba0208e194d
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2023-10-15 21:20:23 +0530

    Add files via upload

---
+++ word2.c
@@ -0,0 +1,31 @@
+//#include <stdio.h>
+//
+//int main() {
+//    int n1, n2, DivSum1 = 0, DivSum2 = 0;
+//
+//    printf("Enter two numbers: ");
+//    scanf("%d %d", &n1, &n2);
+//
+//     Calculate the sum of divisors for n1
+//    for (int i = 1; i <= n1 / 2; i++) {
+//        if (n1 % i == 0) {
+//            DivSum1 += i;
+//        }
+ //   }
+
+    // Calculate the sum of divisors for n2
+//    for (int i = 1; i <= n2 / 2; i++) {
+//        if (n2 % i == 0) {
+//            DivSum2 += i;
+//        }
+//    }
+//
+  //   Check if the numbers are amicable
+//    if (n1 == DivSum2 && n2 == DivSum1) {
+//        printf("%d and %d are Amicable numbers\n", n1, n2);
+//    } else {
+//        printf("%d and %d are not Amicable numbers\n", n1, n2);
+//    }
+//
+//    return 0;
+///}
