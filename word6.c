c9841bfba6e87fc56e7eae99cb54edb1b86e5868
Rishav sahu <145828980+NightxFury@users.noreply.github.com>
2023-10-15 21:21:34 +0530

    Add files via upload

---
+++ word6.c
@@ -0,0 +1,21 @@
+//#include <stdio.h>
+//
+//int main() {
+//    int n;
+//
+//    printf("Enter the number of times to print 'Hello World' (Enter -1 to stop): ");
+//    scanf("%d", &n);
+//
+//    while (n != -1) {
+//        for (int i = 0; i < n; i++) {
+//            printf("Hello World\n");
+//        }
+//
+//        printf("Enter the number of times to print 'Hello World' (Enter -1 to stop): ");
+//        scanf("%d", &n);
+//    }
+//
+//    printf("Stop!\n");
+//
+//    return 0;
+//}
